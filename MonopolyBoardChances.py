a03e553c0a72a04b91fe31b999d248f89256b0ec
tinyriggs <nr107984@gmail.com>
2022-03-27 02:31:25 -0400

    Plots a graph for a 30 turn game.

--- MonopolyBoardChances.py
+++ MonopolyBoardChances.py
@@ -1,12 +1,34 @@
 import random
+import matplotlib.pyplot as plt
 
 spaces_histogram = {}
+x_tick = []
 
 for i in range(1, 41):
     spaces_histogram[i] = 0
+    x_tick.append(i)
 
-for i in range(1,1000):
-    answer = random.randint(1,6) + random.randint(1,6)
+answer = 0
+
+for i in range(1,31):
+    answer = answer + (random.randint(1,6) + random.randint(1,6))
+    if answer > 40:
+        answer = answer - 40
     spaces_histogram[answer] += 1
 
-print(spaces_histogram)
+
+
+def plot_graph():
+    x = []
+    y = []
+    for key in spaces_histogram:
+        x.append(key)
+        y.append(spaces_histogram[key])
+    plt.bar(x, y)
+    plt.xticks(x_tick)
+    plt.xlabel("x - axis")
+    plt.ylabel("y - axis")
+    plt.title("my graph")
+    plt.show()
+
+plot_graph()
